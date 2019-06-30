c550170567969175a699d4294bfd155700301711
Aaron Hennessey <mynamebemrrandom@gmail.com>
2019-06-30 02:21:25 +0100

    font command yay

--- MrBot/cogs/fun.py
+++ MrBot/cogs/fun.py
@@ -1,3 +1,4 @@
+from itertools import zip_longest
 from discord.ext import commands
 from io import BytesIO
 from PIL import Image
@@ -31,6 +32,14 @@ class Fun(commands.Cog):
 	def do_ascii(self, ctx, columns, rows):
 		ascii_image = ascii.loadFromUrl(ctx, columns=columns, rows=rows, color=False)
 		return ascii_image
+
+	def do_newline(self, text, amount):
+		words = text.split(' ')
+		grouped_words = ['  '.join(words[i: i + amount]) for i in range(0, len(words), amount)]
+		newline_text = ''
+		for value in grouped_words:
+			newline_text += f'{value}\n'
+		return newline_text
 
 	@commands.command(name='ascii')
 	async def ascii(self, ctx, columns: typing.Optional[int] = 50, rows: typing.Optional[int] = 25, image: typing.Union[discord.User, discord.Member, str] = None):
@@ -70,9 +79,9 @@ class Fun(commands.Cog):
 		else:
 			ascii_image = await self.bot.loop.run_in_executor(None, self.do_ascii, ctx, 50, 25)
 		if len(ascii_image) > 2000:
-			async with aiofiles.open(f'images/ascii/{ctx.author.id}.txt', mode='w') as f:
+			async with aiofiles.open(f'images/ascii/{ctx.author.id}_ascii.txt', mode='w') as f:
 				await f.write(ascii_image)
-			await ctx.send('The resulting art was too long, so i put it in this text file!', file=discord.File(f'images/ascii/{ctx.author.id}.txt'))
+			await ctx.send('The resulting art was too long, so i put it in this text file!', file=discord.File(f'images/ascii/{ctx.author.id}_ascii.txt'))
 		else:
 			await ctx.send(f'```{ascii_image}```')
 
@@ -80,8 +89,36 @@ class Fun(commands.Cog):
 		return await ctx.send(f'That took {end - start:.3f}sec to complete')
 
 	@commands.command(name='font')
-	async def font(self, ctx):
-		print(0)
+	async def font(self, ctx, font: str = None, *, text: str):
+
+		start = time.perf_counter()
+		await ctx.trigger_typing()
+
+		if not font:
+			ascii_text = art.text2art(self.do_newline(text, 3), chr_ignore=True)
+			if len(ascii_text) > 2000:
+				async with aiofiles.open(f'images/ascii/{ctx.author.id}_font.txt', mode='w') as f:
+					await f.write(ascii_text)
+				await ctx.send('The resulting art was too long, so i put it in this text file!', file=discord.File(f'images/ascii/{ctx.author.id}_font.txt'))
+			else:
+				await ctx.send(f'```{ascii_text}```')
+		else:
+			try:
+				ascii_text = art.text2art(self.do_newline(text, 3),font=font, chr_ignore=True)
+				if len(ascii_text) > 2000:
+					async with aiofiles.open(f'images/ascii/{ctx.author.id}_font.txt', mode='w') as f:
+						await f.write(ascii_text)
+					await ctx.send('The resulting art was too long, so i put it in this text file!', file=discord.File(f'images/ascii/{ctx.author.id}_font.txt'))
+				else:
+					await ctx.send(f'```{ascii_text}```')
+			except art.artError:
+				return await ctx.send(f'That was not a valid font.')
+
+		end = time.perf_counter()
+		return await ctx.send(f'That took {end - start:.3f}sec to complete')
+
+
+
 
 
 def setup(bot):
