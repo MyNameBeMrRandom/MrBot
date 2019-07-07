2ca33f6ebcdd956fc5ea6654f31ae1924ab0c2f4
Aaron Hennessey <mynamebemrrandom@gmail.com>
2019-07-07 22:35:13 +0100

    now using andesite and granitepy

--- MrBot/cogs/bg_tasks.py
+++ MrBot/cogs/bg_tasks.py
@@ -9,8 +9,8 @@ class BgTasks(commands.Cog):
 
 	def __init__(self, bot):
 		self.bot = bot
-		self.presence_task = self.bot.loop.create_task(self.activity_changing())
 		self.dblpy = dbl.Client(self.bot, config.DBL_TOKEN, webhook_path='/dblwebhook', webhook_auth=f'{config.DBL_TOKEN}', webhook_port=5000)
+		self.presence_task = self.bot.loop.create_task(self.activity_changing())
 		self.updating = self.bot.loop.create_task(self.update_guild_count())
 
 	async def activity_changing(self):
@@ -26,10 +26,10 @@ class BgTasks(commands.Cog):
 	async def update_guild_count(self):
 		await self.bot.wait_until_ready()
 		while self.bot.is_ready():
-			self.bot.logging.info('[SERVER_COUNT] - Attempting to post server count.')
+			self.bot.logging.info('[SERVER_COUNT] - Attempting to post server count to DBL.')
 			try:
 				await self.dblpy.post_guild_count()
-				self.bot.logging.info(f'[SERVER_COUNT] - Posted server count ({self.dblpy.guild_count()}).')
+				self.bot.logging.info(f'[SERVER_COUNT] - Posted server count to DBL({self.dblpy.guild_count()}).')
 			except Exception as e:
 				self.bot.logging.exception('[SERVER_COUNT] - Failed to post server count.\n{}: {}'.format(type(e).__name__, e))
 			await asyncio.sleep(21600)
