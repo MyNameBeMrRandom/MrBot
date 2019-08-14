38db9e0aeb103c90dc60ccea1db7350f5f8e8b4c
Aaron Hennessey <mynamebemrrandom@gmail.com>
2019-08-14 15:46:15 +0100

    Rewrote deposit and withdraw commands

--- MrBot/cogs/economy.py
+++ MrBot/cogs/economy.py
@@ -16,19 +16,20 @@ class Economy(commands.Cog):
         Display the amount of money you have.
         """
 
-        if not await self.bot.pool.fetchrow("SELECT key FROM user_config WHERE key = $1", ctx.author.id):
-            return await ctx.send('You dont have an account.')
+        # Check if the user has an account.
         data = await self.bot.pool.fetchrow("SELECT * FROM user_config WHERE key = $1", ctx.author.id)
-        bank = data["bank"]
-        cash = data["cash"]
+        if not data:
+            return await ctx.send("You don't have an account. Use `mb account create` to make one.")
+
+        # Show the user how much money they have.
         embed = discord.Embed(
             colour=0x57FFF5,
             timestamp=ctx.message.created_at,
             title=f"{ctx.author.name}'s balance:"
         )
-        embed.add_field(name='Bank:', value=f'**£{bank}**')
-        embed.add_field(name='Cash:', value=f'**£{cash}**')
-        embed.add_field(name='Total:', value=f'**£{cash + bank}**')
+        embed.add_field(name='Bank:', value=f'**£{data["bank"]}**')
+        embed.add_field(name='Cash:', value=f'**£{data["cash"]}**')
+        embed.add_field(name='Total:', value=f'**£{data["bank"] + data["cash"]}**')
         embed.set_author(icon_url=ctx.author.avatar_url, name=ctx.author.name)
         return await ctx.send(embed=embed)
 
@@ -38,51 +39,34 @@ class Economy(commands.Cog):
         Desposit an amount of money to your bank.
         """
 
-        if not await self.bot.pool.fetchrow("SELECT key FROM user_config WHERE key = $1", ctx.author.id):
-            return await ctx.send('You dont have an account.')
+        # Check if the user has an account.
         data = await self.bot.pool.fetchrow("SELECT * FROM user_config WHERE key = $1", ctx.author.id)
-        bank_amount = data["bank"]
-        cash_amount = data["cash"]
+        if not data:
+            return await ctx.send("You don't have an account. Use `mb account create` to make one.")
+        # If the user wants to deposit all of their money.
         if amount == 'all':
-            if cash_amount == 0:
-                embed = discord.Embed(
-                    colour=0x57FFF5,
-                    timestamp=ctx.message.created_at,
-                    description=f'You have no money to deposit.'
-                )
-                embed.set_author(icon_url=ctx.author.avatar_url, name=ctx.author.name)
-                return await ctx.send(embed=embed)
-            await self.bot.pool.execute(f"UPDATE user_config SET bank = $1 WHERE key = $2", bank_amount + cash_amount, ctx.author.id)
-            await self.bot.pool.execute(f"UPDATE user_config SET cash = $1 WHERE key = $2", cash_amount - cash_amount, ctx.author.id)
-            embed = discord.Embed(
-                colour=0x57FFF5,
-                timestamp=ctx.message.created_at,
-                description=f'Deposited **£{cash_amount}** to your bank.'
-            )
-            embed.set_author(icon_url=ctx.author.avatar_url, name=ctx.author.name)
-            return await ctx.send(embed=embed)
-        else:
-            try:
-                amount = int(amount)
-                if amount > cash_amount or amount <= 0:
-                    embed = discord.Embed(
-                        colour=0x57FFF5,
-                        timestamp=ctx.message.created_at,
-                        description=f'You cant deposit that amount of money, you either dont have enough money or tried to deposit an amount less then or equal to 0'
-                    )
-                    embed.set_author(icon_url=ctx.author.avatar_url, name=ctx.author.name)
-                    return await ctx.send(embed=embed)
-                await self.bot.pool.execute(f"UPDATE user_config SET bank = $1 WHERE key = $2", bank_amount + amount, ctx.author.id)
-                await self.bot.pool.execute(f"UPDATE user_config SET cash = $1 WHERE key = $2", cash_amount - amount, ctx.author.id)
-                embed = discord.Embed(
-                    colour=0x57FFF5,
-                    timestamp=ctx.message.created_at,
-                    description=f'Deposited **£{amount}** to your bank.'
-                )
-                embed.set_author(icon_url=ctx.author.avatar_url, name=ctx.author.name)
-                return await ctx.send(embed=embed)
-            except ValueError:
-                return await ctx.send('That was not a valid amount of money, do not use commas or spaces. e.g `mb deposit 999999`')
+            # If the user has no money in "cash" return.
+            if data["cash"] == 0:
+                return await ctx.send("You have no money to deposit.")
+            # Add the cash money to bank money and then remove all cash money.
+            await self.bot.pool.execute(f"UPDATE user_config SET bank = $1 WHERE key = $2", data["bank"] + data["cash"], ctx.author.id)
+            await self.bot.pool.execute(f"UPDATE user_config SET cash = $1 WHERE key = $2", data["cash"] - data["cash"], ctx.author.id)
+            return await ctx.send(f"Deposited **£{data['cash']}** to your bank.")
+        # If the user want to deposit a specific amount of money. Check if amount is an int.
+        try:
+            amount = int(amount)
+        except ValueError:
+            return await ctx.send('That was not a valid amount of money, do not use commas or spaces. e.g `mb deposit 999999`')
+        # If the amount is lower then or equal to 0.
+        if amount <= 0:
+            return await ctx.send("You can't deposit an amount lower then or equal to 0.")
+        # If the amount is bigger then the amount of "cash" the user has.
+        if amount > data["cash"]:
+            return await ctx.send("You can't deposit an amount of money higher then the amount of cash you have.")
+        # Add the amount to bank and remove amount from cash.
+        await self.bot.pool.execute(f"UPDATE user_config SET bank = $1 WHERE key = $2", data["bank"] + amount, ctx.author.id)
+        await self.bot.pool.execute(f"UPDATE user_config SET cash = $1 WHERE key = $2", data["cash"] - amount, ctx.author.id)
+        return await ctx.send(f'Deposited **£{amount}** to your bank.')
 
     @commands.command(name='withdraw', aliases=['wd'])
     async def withdraw(self, ctx, amount):
@@ -90,57 +74,41 @@ class Economy(commands.Cog):
         Withdraw an amount of money from your bank.
         """
 
-        if not await self.bot.pool.fetchrow("SELECT key FROM user_config WHERE key = $1", ctx.author.id):
-            return await ctx.send('You dont have an account.')
+        # Check if the user has an account.
         data = await self.bot.pool.fetchrow("SELECT * FROM user_config WHERE key = $1", ctx.author.id)
-        bank_amount = data["bank"]
-        cash_amount = data["cash"]
+        if not data:
+            return await ctx.send("You don't have an account. Use `mb account create` to make one.")
+        # If the user wants to withdraw all of their money.
         if amount == 'all':
-            if bank_amount == 0:
-                embed = discord.Embed(
-                    colour=0x57FFF5,
-                    timestamp=ctx.message.created_at,
-                    description=f'You have no money to withdraw.'
-                )
-                embed.set_author(icon_url=ctx.author.avatar_url, name=ctx.author.name)
-                return await ctx.send(embed=embed)
-            await self.bot.pool.execute(f"UPDATE user_config SET cash = $1 WHERE key = $2", cash_amount + bank_amount, ctx.author.id)
-            await self.bot.pool.execute(f"UPDATE user_config SET bank = $1 WHERE key = $2", bank_amount - bank_amount, ctx.author.id)
-            embed = discord.Embed(
-                colour=0x57FFF5,
-                timestamp=ctx.message.created_at,
-                description=f'Withdrew **£{bank_amount}** from your bank.'
-            )
-            embed.set_author(icon_url=ctx.author.avatar_url, name=ctx.author.name)
-            return await ctx.send(embed=embed)
-        elif not amount == 'all':
-            try:
-                amount = int(amount)
-                if amount > bank_amount or amount <= 0:
-                    embed = discord.Embed(
-                        colour=0x57FFF5,
-                        timestamp=ctx.message.created_at,
-                        description=f'You cant withdraw that amount of money, you either dont have enough money or tried to withdraw an amount less then or equal to 0'
-                    )
-                    embed.set_author(icon_url=ctx.author.avatar_url, name=ctx.author.name)
-                    return await ctx.send(embed=embed)
-                await self.bot.pool.execute(f"UPDATE user_config SET cash = $1 WHERE key = $2", cash_amount + amount, ctx.author.id)
-                await self.bot.pool.execute(f"UPDATE user_config SET bank = $1 WHERE key = $2", bank_amount - amount, ctx.author.id)
-                embed = discord.Embed(
-                    colour=0x57FFF5,
-                    timestamp=ctx.message.created_at,
-                    description=f'Withdrew **£{amount}** from your bank.'
-                )
-                embed.set_author(icon_url=ctx.author.avatar_url, name=ctx.author.name)
-                return await ctx.send(embed=embed)
-            except ValueError:
-                return await ctx.send('That was not a valid amount of money, do not use commas or spaces. e.g `mb deposit 999999`')
+            # If the user has no money in "bank" return.
+            if data["bank"] == 0:
+                return await ctx.send("You have no money to withdraw.")
+            # Add the bank amount to cash and remove bank amount from amount.
+            await self.bot.pool.execute(f"UPDATE user_config SET cash = $1 WHERE key = $2", data["cash"] + data["bank"], ctx.author.id)
+            await self.bot.pool.execute(f"UPDATE user_config SET bank = $1 WHERE key = $2", data["bank"] - data["bank"], ctx.author.id)
+            return await ctx.send(f"Withdrew **£{data['bank']}** from your bank.")
+        # If the user want to withdraw a specific amount of money. Check if amount is an int.
+        try:
+            amount = int(amount)
+        except ValueError:
+            return await ctx.send('That was not a valid amount of money, do not use commas or spaces. e.g `mb withdraw 999999`')
+        # If the amount is lower then or equal to 0.
+        if amount <= 0:
+            return await ctx.send("You can't withdraw an amount lower then or equal to 0.")
+        # If the amount is bigger then the amount of "bank" the user has.
+        if amount > data["bank"]:
+            return await ctx.send("You can't withdraw an amount of money higher then the amount of money you have in your bank.")
+        # Add the amount to cash and remove amount from bank.
+        await self.bot.pool.execute(f"UPDATE user_config SET cash = $1 WHERE key = $2", data["cash"] + amount, ctx.author.id)
+        await self.bot.pool.execute(f"UPDATE user_config SET bank = $1 WHERE key = $2", data["bank"] - amount, ctx.author.id)
+        return await ctx.send(f'Withdrew **£{amount}** from your bank.')
+
 
     @commands.command(name='claim')
     async def claim(self, ctx):
 
         if not await self.bot.pool.fetchrow("SELECT key FROM user_config WHERE key = $1", ctx.author.id):
-            return await ctx.send('You dont have an account.')
+            return await ctx.send("You don't have an account.")
 
         # Get users account.
         data = await self.bot.pool.fetchrow("SELECT * FROM user_config WHERE key = $1", ctx.author.id)
