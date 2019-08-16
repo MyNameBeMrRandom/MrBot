b530dafb614f0a9799ad806a43767b2b4c8eed67
Aaron Hennessey <mynamebemrrandom@gmail.com>
2019-08-17 00:09:35 +0100

    added an example config file

---
+++ MrBot/config_example.py
@@ -0,0 +1,23 @@
+DISCORD_TOKEN = ''
+DISCORD_PREFIX = ''
+DBL_TOKEN = ''
+
+# Andesite nodes
+IP_1 = ''
+PORT_1 = 00000
+ADRESS_1 = ''
+PASSWORD_1 = ''
+
+# PSQL
+DB_CONN_INFO = {
+    'user': '',
+    'password': '',
+    'host': '',
+    'database': ''
+}
+
+# Windows Opus path
+OPUS = ""
+
+# Linux Opus path
+#OPUS = ""
