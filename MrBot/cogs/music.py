b19d27606e42c74b518509c60269c0f396c60324
Aaron Hennessey <mynamebemrrandom@gmail.com>
2019-09-29 18:23:07 +0100

    oh

--- MrBot/cogs/music.py
+++ MrBot/cogs/music.py
@@ -5,31 +5,9 @@ import random
 import andesite
 import discord
 from discord.ext import commands
-from youtube_dl import YoutubeDL
 
 from .utils import formatting
 from .utils.queue import Queue
-
-ytdlopts = {
-    'format': 'bestaudio',
-    'outtmpl': 'files/voice/%(id)s.%(ext)s',
-    'nocheckcertificate': True,
-    'ignoreerrors': False,
-    'logtostderr': False,
-    'quiet': True,
-    'no_warnings': True,
-    'default_search': 'auto',
-    'source_address': '0.0.0.0',
-    'postprocessors':
-        [
-            {
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'mp3',
-                'preferredquality': '192',
-            }
-        ],
-}
-ytdl = YoutubeDL(ytdlopts)
 
 
 class Track(andesite.Track):
@@ -382,65 +360,6 @@ class Music(commands.Cog):
 
         # Send the embed
         await ctx.send(embed=embed)
-
-    @commands.command(name="download")
-    async def download(self, ctx):
-        """
-        Downloads an mp3 version of the current track.
-        """
-
-        # If the player is not connected then do nothing
-        if not ctx.player.is_connected:
-            return await ctx.send(f"MrBot is not connected to any voice channels.")
-
-        # If the user is not a voice channel.
-        if not ctx.author.voice or not ctx.author.voice.channel:
-            return await ctx.send(f"You must be in a voice channel to use this command.")
-
-        # If the user is not in the same voice channel as the bot.
-        if ctx.player.channel_id != ctx.author.voice.channel.id:
-            return await ctx.send(f"You must be in the same voice channel as me to use this command.")
-
-        # If nothing is current playing.
-        if not ctx.player.current:
-            return await ctx.send("No tracks currently playing.")
-
-        # Start process
-        message = await ctx.send("Downloading track... <a:downloading:616426925350715393>")
-        await ctx.trigger_typing()
-
-        # Download the track in an executor, cuz this is blocking.
-        track_title, track_id = await self.bot.loop.run_in_executor(None, self.do_download, ctx)
-
-        if ctx.guild.premium_tier == 1 or ctx.guild.premium_tier == 0:
-            size = os.path.getsize(f"files/voice/{track_id}.mp3")
-            if size >= 8388608:
-                os.remove(f"files/voice/{track_id}.mp3")
-                return await ctx.send("This track is too big to upload to discord.")
-        if ctx.guild.premium_tier == 2:
-            size = os.path.getsize(f"files/voice/{track_id}.mp3")
-            if size >= 52428800:
-                os.remove(f"files/voice/{track_id}.mp3")
-                return await ctx.send("This track is too big to upload to discord.")
-        if ctx.guild.premium_tier == 3:
-            size = os.path.getsize(f"files/voice/{track_id}.mp3")
-            if size >= 104857600:
-                os.remove(f"files/voice/{track_id}.mp3")
-                return await ctx.send("This track is too big to upload to discord.")
-
-        await message.edit(content="Uploading track... <a:downloading:616426925350715393>")
-
-        # Upload the file
-        await ctx.send(content="Here is your download.", file=discord.File(filename=f"{track_title}.mp3", fp=f"files/voice/{track_id}.mp3"))
-
-        # Delete the file and message
-        os.remove(f"files/voice/{track_id}.mp3")
-        return await message.delete()
-
-    def do_download(self, ctx):
-        data = ytdl.extract_info(f"{ctx.player.current.uri}", download=False)
-        ytdl.download([f"{ctx.player.current.uri}"])
-        return data["title"], data["id"]
 
     @commands.command(name="volume", aliases=["vol"])
     async def volume(self, ctx, volume: int = None):
